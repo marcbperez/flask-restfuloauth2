6845a3e944721296c182830f624ef310732a845b
marcbperez <marcbperez@users.noreply.github.com>
2017-01-01 16:57:51 +0000

    Added etag, created and updated fields.

--- restfuloauth2/todo/endpoint.py
+++ restfuloauth2/todo/endpoint.py
@@ -1,108 +1,73 @@
 from flask import request
-from flask_restful import abort, Resource, reqparse
+from flask_restful import abort, Resource
 from . import Todo
-from ..database import db
-from ..oauth.models import Token, User
+from ..oauth.models import User
 
 
-def get_todo_or_abort(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
+def get_todo_or_abort(todo_id, user):
+    todo = Todo.get_permitted_todo(todo_id, user)
     if not todo:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+        abort(404, message="Item not found")
+
     return todo
 
 
-def parse_todo_arguments():
-    parser = reqparse.RequestParser()
-    parser.add_argument('public', required=True, help='The todo visibility.')
-    parser.add_argument('description', required=True, help='What to do.')
-    parser.add_argument('done', required=True, help='If is completed or not.')
-    return parser.parse_args(strict=True)
+def operation_or_etag_not_matching(operation):
+    if not operation:
+        abort(401, message="Etags do not match")
 
 
-def serialize_todo(todo):
-    return {
-        'id': todo.id,
-        'user_id': todo.user_id,
-        'public': todo.public,
-        'description': todo.description,
-        'done': todo.done,
-    }
-
-
-def serialize_todos(todos):
-    _todos = []
-    for todo in todos:
-        _todos.append(serialize_todo(todo))
-    return _todos
-
-
-def get_current_user():
-    access_token = request.oauth.headers.get('Authorization').split(' ')[1]
-    token = Token.find(access_token)
-    return token.user
-
-
-def is_permitted_or_abort(todo, user):
-    if todo.public or (todo.user_id == user.id):
-        return True
-    abort(401, message="Unauthorized request")
+def get_sort_attribute(sort):
+    sort_column = sort.split('-')[0]
+    sort_direction = sort.split('-')[1]
+    sort_attribute = getattr(Todo, sort_column)
+    return getattr(sort_attribute, sort_direction)()
 
 
 class TodoItem(Resource):
     def get(self, todo_id):
-        todo = get_todo_or_abort(todo_id)
-        user = get_current_user()
-        is_permitted_or_abort(todo, user)
-        return serialize_todo(todo)
+        user = User.get_authorized()
+        todo = get_todo_or_abort(todo_id, user)
+
+        return todo.serialize()
 
     def delete(self, todo_id):
-        todo = get_todo_or_abort(todo_id)
-        user = get_current_user()
-        is_permitted_or_abort(todo, user)
-        db.session.delete(todo)
-        db.session.commit()
+        user = User.get_authorized()
+        todo = get_todo_or_abort(todo_id, user)
+        args = Todo.parse_delete_arguments()
+        delete = Todo.delete(todo, args['etag'])
+        operation_or_etag_not_matching(delete)
+
         return '', 204
 
     def put(self, todo_id):
-        todo = get_todo_or_abort(todo_id)
-        user = get_current_user()
-        is_permitted_or_abort(todo, user)
-        args = parse_todo_arguments()
-        todo.public = args['public'] == '1'
-        todo.description = args['description']
-        todo.done = args['done'] == '1'
-        db.session.commit()
-        return serialize_todo(todo), 201
+        user = User.get_authorized()
+        todo = get_todo_or_abort(todo_id, user)
+        args = Todo.parse_put_arguments()
+        update = Todo.update(todo, args['etag'], args['public'],
+            args['description'], args['done'])
+        operation_or_etag_not_matching(update)
+
+        return update.serialize(), 201
 
 
 class TodoIndex(Resource):
     def get(self):
-        max_results = request.args.get('max_results', '10')
         page = request.args.get('page', '1')
-        sort_column = request.args.get('sort', 'id-asc').split('-')[0]
-        sort_direction = request.args.get('sort', 'id-asc').split('-')[1]
-        sort_attr = getattr(Todo, sort_column)
-        sort_direction_attr = getattr(sort_attr, sort_direction)()
-        todos = Todo.query.order_by(sort_direction_attr).paginate(int(page),
-            int(max_results), error_out=False).items
-        user = get_current_user()
-        permitted_todos = []
+        max_results = request.args.get('max_results', '10')
+        sort = request.args.get('sort', 'id-asc')
+        sort_direction_attribute = get_sort_attribute(sort)
 
-        for todo in todos:
-            if todo.public or (todo.user_id == user.id):
-                permitted_todos.append(todo)
+        user = User.get_authorized()
+        todos = Todo.get_permitted_todos(user, sort_direction_attribute, page,
+            max_results)
 
-        return serialize_todos(permitted_todos)
+        return Todo.serialize_list(todos)
 
     def post(self):
-        todo = Todo()
-        user = get_current_user()
-        args = parse_todo_arguments()
-        todo.user_id = user.id
-        todo.public = args['public'] == '1'
-        todo.description = args['description']
-        todo.done = args['done'] == '1'
-        db.session.add(todo)
-        db.session.commit()
-        return serialize_todo(todo), 201
+        user = User.get_authorized()
+        args = Todo.parse_post_arguments()
+        todo = Todo.create(user, args['public'], args['description'],
+            args['done'])
+
+        return todo.serialize(), 201
