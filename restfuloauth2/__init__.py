f54aecab1340d9dd04845ee35c8af62377fe9f0a
marcbperez <marcbperez@users.noreply.github.com>
2017-07-05 21:11:54 +0100

    Added database migration control.

--- restfuloauth2/__init__.py
+++ restfuloauth2/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask, redirect, url_for
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_cors import CORS
 from . import config
 from database import db
-from oauth import provider
+from oauth import oauth, provider
 from oauth.validator import RequestValidator
-from oauth import oauth
+from flask_migrate import Migrate
 from flask_restful import Api
+from flasgger import Swagger
 from dummy.endpoint import DummyIndex, DummyItem
 
 
@@ -13,7 +14,8 @@ def register_extensions(app):
     """Registers application extensions."""
     # Register and initialize the database.
     db.init_app(app)
-    db.create_all(app=app)
+    # Register and initialize the database migrations.
+    Migrate(app, db)
     # Register Oauth2/REST provider.
     provider.init_app(app)
     provider._validator = RequestValidator()
@@ -25,11 +27,16 @@ def register_blueprints(app):
     app.register_blueprint(oauth, url_prefix='/v1/oauth')
     # Register REST model endpoints.
     api = Api(app, decorators=[provider.require_oauth()])
+    # Dummy REST endpoints.
     api.add_resource(DummyIndex, '/v1/dummy')
     api.add_resource(DummyItem, '/v1/dummy/<dummy_id>')
+    # Register Swagger API docs.
+    Swagger(
+        app, config=app.config['SWAGGER_CONFIG'],
+        template=app.config['SWAGGER_TEMPLATE'])
 
 
-def create_app(config=config.base_config):
+def create_app(config=config.BaseConfig):
     """Configure and return the main app."""
     app = Flask(__name__)
     # Setup with the provided configuration object.
@@ -45,9 +52,3 @@ def create_app(config=config.base_config):
 
 app = create_app()
 """Main program app."""
-
-
-@app.route('/')
-def default():
-    """Default route, redirects to management module."""
-    return redirect(url_for('oauth.management'))
