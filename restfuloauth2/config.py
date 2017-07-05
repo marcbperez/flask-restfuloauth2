42fee2617b8cf57dfde33bd0eb22a66909bcf72d
marcbperez <marcbperez@users.noreply.github.com>
2017-07-05 21:11:54 +0100

    Added API manifest with Swagger.

--- restfuloauth2/config.py
+++ restfuloauth2/config.py
@@ -1,20 +1,35 @@
 import os
 
 
-class base_config(object):
+class BaseConfig(object):
     """Base configuration object."""
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    SWAGGER = {
+        'title': 'RestfulOauth2 Service API documentation',
+        'uiversion': 3,
+    }
 
-class dev_config(base_config):
-    """Development configuration object, extends base."""
-    DEBUG = True
-    ASSETS_DEBUG = True
-    WTF_CSRF_ENABLED = False
+    SWAGGER_TEMPLATE = {
+        'info': {
+            'title': 'RestfulOauth2',
+            'description': 'Service API documentation',
+            'version': '0.1.2',
+        },
+        'basePath': '/',
+    }
 
-
-class test_config(base_config):
-    """Testing configuration objecet, extends base."""
-    TESTING = True
-    WTF_CSRF_ENABLED = False
+    SWAGGER_CONFIG = {
+        'headers': [],
+        'specs': [
+            {
+                'endpoint': 'RestfulOauth2',
+                'route': '/',
+            }
+        ],
+        'static_url_path': '/flasgger_static',
+        'swagger_ui': True,
+        'specs_route': '/api/',
+    }
